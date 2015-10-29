dcce92b6c561337cb8a85a9547209ffccfc1273d
Mario Cecchi <macecchi@gmail.com>
2015-10-29 15:51:41 -0200

    Using local data instead of API.

--- utils.js
+++ utils.js
@@ -1,16 +1,32 @@
-var getJSON = function(url) {
-  return new Promise(function(resolve, reject) {
-    var xhr = new XMLHttpRequest();
-    xhr.open('get', url, true);
-    xhr.responseType = 'json';
-    xhr.onload = function() {
-      var status = xhr.status;
-      if (status == 200) {
-        resolve(xhr.response);
-      } else {
-        reject(status);
-      }
-    };
-    xhr.send();
-  });
+function getJSON(url) {
+	return new Promise(function(resolve, reject) {
+		var xhr = new XMLHttpRequest();
+		xhr.open('get', url, true);
+		xhr.responseType = 'json';
+		xhr.onload = function() {
+			var status = xhr.status;
+			if (status == 200) {
+				resolve(xhr.response);
+			} else {
+				reject(status);
+			}
+		};
+		xhr.send();
+	});
 };
+
+function addHeatSamples(data) {
+	console.log('Total samples: ' + data.query_result.data.rows.length);
+
+	var layerSamples = [];
+	var maxIntensity = 0;
+	data.query_result.data.rows.forEach(function(sample) {
+		layerSamples.push([sample.lat, sample.long, sample.amostra]);
+		maxIntensity = Math.max(maxIntensity, sample.amostra);
+	});
+
+	console.log('Total samples: ' + data.query_result.data.rows.length);
+	console.log('Max sample: ' + maxIntensity);
+
+	var heat = L.heatLayer(layerSamples, {max: maxIntensity}).addTo(map);
+}
